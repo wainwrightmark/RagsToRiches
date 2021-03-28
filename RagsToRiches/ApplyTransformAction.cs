f24f740a5daa0671f86e851e000988bf1ac17864
Mark Wainwright <wainwrightml@gmail.com>
2021-03-28 19:18:20 +0100

    Added cheat button / action

--- RagsToRiches/ApplyTransformAction.cs
+++ RagsToRiches/ApplyTransformAction.cs
@@ -1,4 +1,8 @@
-﻿namespace RagsToRiches
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace RagsToRiches
 {
     public record ApplyTransformAction(TransformType Transform, int Index, bool Start) : IAction<GameState>
     {
@@ -6,6 +10,34 @@
         public GameState Act(GameState state)
         {
             return state.TryApply(Transform, Index, Start) ?? state;
+        }
+    }
+
+    public record CheatAction : IAction<GameState>
+    {
+        /// <inheritdoc />
+        public GameState Act(GameState state)
+        {
+            var solution = Solver.SolveGame(state.Start, state.Finish, TransformType.All);
+
+            var transforms = solution.Skip(1).Select(x => new Transform(TransformType.Increment.Instance, x)); //cheeky
+
+            //var previous = state.Start;
+
+            //foreach (var i in solution)
+            //{
+            //    var t = TransformType.All.First(x => x.Function(previous) == i);
+            //    transforms.Add(new Transform(t, i));
+            //    previous = i;
+            //}
+
+
+            state = state with
+            {
+                StartTransforms = transforms.ToImmutableList(), FinishTransforms = ImmutableList<Transform>.Empty
+            };
+
+            return state;
         }
     }
 }
